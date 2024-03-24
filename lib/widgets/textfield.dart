ea1d6834862390da4889a6f5c19e76f5d9bddcf5
Rohan Vitta <138205850+rohan-vitta@users.noreply.github.com>
2024-03-24 10:03:22 +0530

    Add files via upload

---
+++ lib/widgets/textfield.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+      child: TextField(
+        controller: controller,
+        decoration: const InputDecoration(
+            border: InputBorder.none, fillColor: Colors.black, filled: true),
+        style: const TextStyle(fontSize: 50),
+        readOnly: true,
+        autofocus: true,
+        showCursor: true,
+      ),
+    );
+  }
+}
